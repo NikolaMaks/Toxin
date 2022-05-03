53825277f0faa8f9b3263d85efa486ce91529ec7
Maxim <nikolaenkomak@yandex.ru>
2022-05-03 20:08:09 +0300

    new tox

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-console.log(hello);
+import "./style.scss";
+import "./index.pug";
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,60 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
-      },
-  };
+    },
+    module: {
+        rules: [{
+                test: /\.css$/i,
+                use: [
+
+                    "style-loader",
+                    "css-loader"
+                ],
+            },
+            {
+                test: /\.pug$/,
+                use: ['pug-loader']
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+                options: {
+                    // Disables attributes processing
+                    sources: false,
+                },
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    // Creates `style` nodes from JS strings
+                    //   "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({}),
+        new HtmlWebpackPlugin({
+            template: "./src/index.pug"
+        }, ),
+        new CopyPlugin({
+          patterns: [
+            { from: "src/img", to: 'img' }
+          ],
+        }),
+    ],
+}
